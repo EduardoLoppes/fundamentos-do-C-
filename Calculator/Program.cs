4f909da40e71ae0c790ca70c35d81de055ba52ba
EduardoLoppes <mcfgonzalles@gmail.com>
2023-08-03 10:00:47 -0300

    Função adicionada na calculadora: Multiplicação

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,6 +9,7 @@ namespace Calculator
             Soma();
             Subtração();
             Divisão();
+            Multiplicação();
         }
         static void Soma()
         {
@@ -56,6 +57,22 @@ namespace Calculator
             float result = v1/v2;
 
             Console.WriteLine($"O resultado da divisão é: {result}");
+            Console.ReadKey();
+        }
+        static void Multiplicação(){
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor:");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            Console.WriteLine("Segundo valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float result = v1*v2;
+
+            Console.WriteLine($"O resultado da multiplicação é: {result}");
             Console.ReadKey();
         }
     }
